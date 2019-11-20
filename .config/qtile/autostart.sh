d764f3fcdae3605fe55adb833edd6216e83162b3
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2019-11-20 14:09:49 +0000

    Add qtile autostart

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+picom &
+xcape -e 'Super_L=Control_L|Escape' &
+xrdb -merge ~/.config/X11/Xresources &
+
+udiskie --tray --no-config &
+volumeicon &
+nm-applet &
+xfce4-power-manager &
+
+nitrogen --restore &
+
+urxvtd -q -o -f &
+emacs --daemon
