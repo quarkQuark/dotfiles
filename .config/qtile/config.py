7bab1dedf0ceb809fae45586772eafafd07b03c2
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2019-11-20 14:10:10 +0000

    Add line defining modkey

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -16,6 +16,8 @@
 # The above copyright notice and this permission notice shall be included in all copies
 # or substantial portions of the Software.
 
+mod = "mod4"
+
 ##### IMPORTS #####
 
 import os
