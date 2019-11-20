04a0abeccc34837fc2c3b068649de418d43311ed
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2019-11-20 21:01:59 +0000

    Fix undefined functions

--- .config/qtile/keys.py
+++ .config/qtile/keys.py
@@ -1,3 +1,14 @@
+@lazy.function
+def window_to_prev_group(qtile):
+    if qtile.currentWindow is not None:
+        i = qtile.groups.index(qtile.currentGroup)
+        qtile.currentWindow.togroup(qtile.groups[i - 1].name)
+
+@lazy.function def window_to_next_group(qtile):
+    if qtile.currentWindow is not None:
+        i = qtile.groups.index(qtile.currentGroup)
+        qtile.currentWindow.togroup(qtile.groups[i + 1].name)
+
 def init_keys():
     keys = [
             Key(
