f6eeabacc5baa07b23e950e95b38e74a845277d8
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2019-11-23 15:02:10 +0000

    Variable definitions moved to widgets.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,4 +1,5 @@
 # DEBUGGING: $ qtile -l ERROR (lowercase L)
+#   Then restart with $ qtile-cmd -o cmd -f restart
 #
 # The following comments are the copyright and licensing information from the default
 # qtile config. Copyright (c) 2010 Aldo Cortesi, 2010, 2014 dequis, 2012 Randall Ma,
@@ -21,14 +22,20 @@
 import os
 import subprocess
 import socket
+
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
 from libqtile import layout, bar, widget, hook
+from libqtile.widget import Spacer
+
+from powerline.bindings.qtile.widget import PowerlineTextBox
 
 from typing import List  # noqa: F401
 
 ## My modules
 from keys import init_keys
+from widgets import init_widget_defaults, init_widget_list
+import powerline_widgets
 
 ########################################
 
@@ -65,168 +72,16 @@ layouts = [
            ]
 
 ########################################
-## => PANEL
+## => BAR
 ########################################
 
-bar_colours = [["#292D3E", "#292D3E"], # panel background
-               ["#434758", "#434758"], # background for current screen tab
-               ["#D0D0D0", "#D0D0D0"], # font color for group names
-               ["#F07178", "#F07178"], # background color for layout widget
-               ["#000000", "#000000"], # background for other screen tabs
-               ["#AD69AF", "#AD69AF"], # dark green gradiant for other screen tabs
-               ["#C3E88D", "#C3E88D"], # background color for network widget
-               ["#C792EA", "#C792EA"], # background color for pacman widget
-               ["#9CC4FF", "#9CC4FF"], # background color for cmus widget
-               ["#000000", "#000000"], # background color for clock widget
-               ["#434758", "#434758"]] # background color for systray widget
-
-widget_defaults = dict(
-                    font='Ubuntu Mono',
-                    fontsize=12,
-                    padding=2,
-                    background=bar_colours[2]
-                    )
+widget_defaults = init_widget_defaults()
 extension_defaults = widget_defaults.copy()
 
-prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
-widget_list = [
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 6,
-                    foreground = bar_colours[2],
-                    background = bar_colours[0]
-                    ),
-                widget.GroupBox(
-                    fontsize = 12,
-                    margin_y = 0,
-                    margin_x = 0,
-                    padding_y = 5,
-                    padding_x = 5,
-                    borderwidth = 1,
-                    active = bar_colours[2],
-                    inactive = bar_colours[2],
-                    rounded = False,
-                    highlight_method = "block",
-                    this_current_screen_border = bar_colours[1],
-                    this_screen_border = bar_colours [4],
-                    other_current_screen_border = bar_colours[0],
-                    other_screen_border = bar_colours[0],
-                    foreground = bar_colours[2],
-                    background = bar_colours[0],
-                    hide_unused = True
-                    ),
-                widget.Prompt(
-                    prompt=prompt,
-                    font="Ubuntu Mono",
-                    padding=10,
-                    foreground = bar_colours[3],
-                    background = bar_colours[1]
-                    ),
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 10,
-                    foreground = bar_colours[2],
-                    background = bar_colours[0]
-                    ),
-                widget.WindowName(font="Ubuntu",
-                    fontsize = 12,
-                    foreground = bar_colours[5],
-                    background = bar_colours[0],
-                    padding = 5
-                    ), 
-                widget.Image(
-                    scale = True,
-                    filename = "~/.config/qtile/bar06.png",
-                    background = bar_colours[6]
-                    ),
-                widget.Image(
-                    scale = True,
-                    filename = "~/.config/qtile/bar02-b.png",
-                    backgrund = bar_colours[6]
-                    ),
-                widget.Image(
-                    scale = True,
-                    filename = "~/.config/qtile/bar03.png",
-                    background = bar_colours[3]
-                    ),
-                widget.CurrentLayoutIcon(
-                    scale = 0.6,
-                    foreground=bar_colours[0],
-                    background=bar_colours[3],
-                    ),
-                widget.CurrentLayout(
-                    foreground = bar_colours[0],
-                    background = bar_colours[3],
-                    padding = 5
-                    ),
-                widget.Image(
-                    scale = True,
-                    filename = "~/.config/qtile/bar04.png",
-                    background = bar_colours[7]
-                    ),
-                widget.TextBox(
-                    font="Ubuntu Bold",
-                    text=" ⟳",
-                    padding = 5,
-                    foreground=bar_colours[0],
-                    background=bar_colours[7],
-                    fontsize=14
-                    ),
-                widget.Pacman(
-                    execute = "urxvtc",
-                    update_interval = 1800,
-                    foreground = bar_colours[0],
-                    background = bar_colours[7]
-                    ),
-                widget.TextBox(
-                    text="Updates",
-                    padding = 5,
-                    foreground=bar_colours[0],
-                    background=bar_colours[7]
-                    ),
-                widget.Image(
-                    scale = True,
-                    filename = "~/.config/qtile/bar05.png",
-                    background = bar_colours[8]
-                    ),
-                widget.Image(
-                    scale = True,
-                    filename = "~/.config/qtile/bar07.png",
-                    background = bar_colours[9]
-                    ),
-                widget.Systray(
-                    background=bar_colours[9],
-                    padding = 5
-                    ),
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 6,
-                    foreground = bar_colours[0],
-                    background = bar_colours[9]
-                    ),
-                widget.TextBox(
-                    font="Ubuntu Bold",
-                    text=" 🕒",
-                    foreground=bar_colours[2],
-                    background=bar_colours[9],
-                    padding = 5,
-                    fontsize=14
-                    ),
-                widget.Clock(
-                    foreground = bar_colours[2],
-                    background = bar_colours[9],
-                    format="%A, %d %B - %H:%M"
-                    ),
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 5,
-                    foreground = bar_colours[0],
-                    background = bar_colours[9]
-                    ),
-                ]
+widget_list = init_widget_list()
 
 screens = [
-           Screen(top=bar.Bar(widgets=widget_list, opacity=0.95, size=20))
+           Screen(top=bar.Bar(widgets=widget_list, opacity=0.95, size=25))
            ]
 
 ########################################
