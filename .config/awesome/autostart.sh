90a6fec356e8a79bcc84c22988eeaf59e519495f
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2020-03-21 18:01:19 +0000

    Fix autostart to only run once

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env sh
 
-picom &
-nitrogen --restore &
-xrdb -merge "$HOME/.config/X11/Xresources"
-urxvtd -q -o -f &
-emacs --daemon &
-udiskie --tray --no-config &
-volumeicon &
-nm-applet &
-xfce4-power-manager &
+# run runs programs only if they are not already running
+function run {
+    if ! pgrep $1 > /dev/null ;
+    then
+        $@&
+    fi
+}
+
+run picom
+run nitrogen --restore
+run xcape -e 'Super_L=Control_L|Escape'
+run xrdb -merge "$HOME/.config/X11/Xresources"
+run urxvtd -q -o -f
+run emacs --daemon
+run udiskie --tray --no-config
+run volumeicon
+run nm-applet
+run xfce4-power-manager
