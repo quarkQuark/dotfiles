e511c119bfe9694f0cd192a8b6d5520a3d24c4f8
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2020-05-07 15:26:53 +0100

    Add slight black tint to bar

--- .config/xmonad/src/xmobarrc.hs
+++ .config/xmonad/src/xmobarrc.hs
@@ -1,7 +1,8 @@
 Config { position = Top
        , font     = "xft:Source Code Pro:size=11:bold:antialias=true"
        , fgColor = "#ffffff"
-       , alpha = 0  -- 0 transparent, 255 opaque
+       , bgColor = "#000000"
+       , alpha = 50  -- 0 transparent, 255 opaque
        , sepChar = "%"
        , alignSep = "}{"
        , commands = [ Run StdinReader
