3203e5e9f5b8f8f3aa2390c65baa2c0e39c5d0ad
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2020-08-23 21:27:44 +0100

    Autorandr commented

--- .config/xmonad/src/autostart.sh
+++ .config/xmonad/src/autostart.sh
@@ -21,4 +21,4 @@ pasystray &  # Works for IvanMalison
 # Settings
 xrdb -merge "$HOME/.config/X11/Xresources" &
 $HOME/.config/xmonad/src/modifiers.sh &
-command -v autorandr >/dev/null && autorandr --change &
+#command -v autorandr >/dev/null && autorandr --change
