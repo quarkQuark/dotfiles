c866885be7d13827a8f6435eb9621f3a0eb43345
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2020-12-13 23:24:32 +0000

    Begin moving layoutHook to new module

--- .config/xmonad/app/Main.hs
+++ .config/xmonad/app/Main.hs
@@ -4,11 +4,6 @@ import XMonad                             -- standard xmonad library
 import XMonad.Config.Desktop              -- default desktopConfig
 import XMonad.Hooks.EwmhDesktops          -- Fixes the automatic fullscreening of applications
 import XMonad.Hooks.ManageDocks           -- Manipulate and avoid docks and panels
-import XMonad.Layout.NoBorders            -- Remove borders when unnecessary (smartBorders)
-import XMonad.Layout.Renamed              -- Rename layouts
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.Spacing              -- Gaps around windows
-import XMonad.Layout.ThreeColumns
 import XMonad.Util.NamedActions (addDescrKeys')
 import XMonad.Util.SpawnOnce (spawnOnce)  -- For running autostart only once (on login)
 
@@ -16,6 +11,10 @@ import MyPrograms
 import MyKeys
 import MyCheatsheet
 import MyBar
+import MyLayoutHook
+
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Monocle
 
 -- I want to figure out how window decorations work, but my Haskell is not yet good enough
 --import XMonad.Layout.Decoration
@@ -24,16 +23,6 @@ import MyBar
 
 --------------------------------------------------------------------------------
 -- AESTHETICS
-
--- Gaps around and between windows
--- Changes only seem to apply if I log out then in again
--- Dimensions are given as (Border top bottom right left)
-mySpacing = spacingRaw True                -- Only for >1 window
-                       -- The bottom edge seems to look narrower than it is
-                       (Border 0 15 10 10) -- Size of screen edge gaps
-                       True                -- Enable screen edge gaps
-                       (Border 5 5 5 5)    -- Size of window gaps
-                       True                -- Enable window gaps
 
 myBorderWidth :: Dimension
 myBorderWidth = 2
@@ -46,27 +35,6 @@ myFocusedBorderColour = "#268bd2"
 --mySideDecorationTheme = def
 --mySideDecorate :: Eq a => l a -> ModifiedLayout (Decoration SideDecoration DefaultShrinker) l a
 --mySideDecorate = decoration shrinkText mySideDecorationTheme (SideDecoration L)
-
---------------------------------------------------------------------------------
--- LAYOUTHOOK
-
-myLayouts = tall ||| three ||| wide ||| Full
-    where
-        tall  = renamed [Replace "Tall"]
-              $ mySpacing
-              $ ResizableTall 1 (3/100) (1/2) []
-
-        three = renamed [Replace "Three"]
-              $ mySpacing
-              $ ThreeColMid 1 (3/100) (1/2)
-
-        wide  = renamed [Replace "Wide"]
-              $ Mirror tall
-
-myLayoutHook = avoidStruts
-             $ smartBorders
---           $ mySideDecorate  -- Messes up everything - I don't yet understand why
-               myLayouts
 
 --------------------------------------------------------------------------------
 -- WORKSPACES
@@ -121,7 +89,8 @@ myConfig barProc = desktopConfig
         , focusedBorderColor = myFocusedBorderColour
         -- Hooks
         , manageHook         = myManageHook
-        , layoutHook         = myLayoutHook
+        -- Remove 'avoidStruts' once I get it working in the MyLayoutHook module
+        , layoutHook         = avoidStruts myLayoutHook
         , logHook            = myLogHook barProc
         , workspaces         = myWorkspaces
         , startupHook        = do
---
+++ .config/xmonad/src/MyLayoutHook.hs
@@ -0,0 +1,40 @@
+module MyLayoutHook
+(myLayoutHook)
+where
+
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout
+import XMonad.Layout.NoBorders            -- Remove borders when unnecessary (smartBorders)
+import XMonad.Layout.Renamed              -- Rename layouts
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Spacing              -- Gaps around windows
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.LayoutModifier
+
+-- Gaps around and between windows
+-- Changes only seem to apply if I log out then in again
+-- Dimensions are given as (Border top bottom right left)
+mySpacing = spacingRaw True                -- Only for >1 window
+                       -- The bottom edge seems to look narrower than it is
+                       (Border 0 15 10 10) -- Size of screen edge gaps
+                       True                -- Enable screen edge gaps
+                       (Border 5 5 5 5)    -- Size of window gaps
+                       True                -- Enable window gaps
+
+tall  = renamed [Replace "Tall"]
+      $ mySpacing
+      $ ResizableTall 1 (3/100) (1/2) []
+
+three = renamed [Replace "Three"]
+      $ mySpacing
+      $ ThreeColMid 1 (3/100) (1/2)
+
+wide  = renamed [Replace "Wide"]
+      $ Mirror tall
+
+myLayoutHook =
+              -- uncommenting either of the following two lines gives an ambiguous tyoe error
+              -- avoidStruts
+              -- $ smartBorders
+--           $ mySideDecorate  -- Messes up everything - I don't yet understand why
+              tall ||| three ||| wide ||| Full
