0a36b28de000a54e03d82de5df5ce211686bd411
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2020-12-13 18:09:00 +0000

    Simplify structure of manageHook section

--- .config/xmonad/app/Main.hs
+++ .config/xmonad/app/Main.hs
@@ -63,21 +63,22 @@ myFocusedBorderColour = "#268bd2"
 -- MANAGEHOOK
 -- To find a window class or title, run xprop in a terminal, then click on it
 
+manageSpecific :: ManageHook
+manageSpecific = composeAll . concat $
+    -- Windows to automatically float
+    [ [ className =? c                                    --> doFloat | c <- myFloatClasses ]
+    , [ title     =? t                                    --> doFloat | t <- myFloatTitles ]
+    , [ className =? "zoom" <&&> fmap (isInfixOf z) title --> doFloat | z <- myZoomFloats ]
+    , [ className =? "zoom" <&&> title =? "zoom"          --> doFloat ] -- Zoom notification popups
+    ]
+    where
+        myFloatClasses = ["Gimp", "conky", "plasmashell", "vlc", "Nitrogen", "Tint2conf"]
+        myFloatTitles  = ["Whisker Menu"]
+        -- This allows annoying classes such as "Participants (n)" where n is the number of people
+        myZoomFloats  = ["Chat", "Participants", "Rooms"] -- Currently untested for breakout rooms
+
 myManageHook :: ManageHook
 myManageHook = manageDocks <+> manageHook desktopConfig <+> manageSpecific
-    where
-        manageSpecific = composeAll . concat $
-            -- Windows to automatically float
-            [ [ className =? c                                    --> doFloat | c <- myFloatClasses ]
-            , [ title     =? t                                    --> doFloat | t <- myFloatTitles ]
-            , [ className =? "zoom" <&&> fmap (isInfixOf z) title --> doFloat | z <- myZoomFloats ]
-            , [ className =? "zoom" <&&> title =? "zoom"          --> doFloat ] -- Zoom notification popups
-            ]
-            where
-                myFloatClasses = ["Gimp", "conky", "plasmashell", "vlc", "Nitrogen", "Tint2conf"]
-                myFloatTitles  = ["Whisker Menu"]
-                -- This allows annoying classes such as "Participants (n)" where n is the number of people
-                myZoomFloats  = ["Chat", "Participants", "Rooms"] -- Currently untested for breakout rooms
 
 --------------------------------------------------------------------------------
 -- WORKSPACES
