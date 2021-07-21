33d8019f849fa8bc29d5673e59ea9fd2f6a30a28
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2021-07-21 16:12:27 +0100

    Fix smartparens

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -163,21 +163,11 @@
   :bind-keymap ("C-c p" . projectile-command-map))
 
 (use-package smartparens
-  :hook (prog-mode . smartparens-mode)
-  :config (smartparens-strict-mode))
+  :hook
+  (prog-mode . smartparens-mode)
+  :config
+  (require 'smartparens-config)
+  (add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode))
 
 (use-package evil-cleverparens
   :hook (emacs-lisp-mode . evil-cleverparens-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(undo-tree which-key use-package solaire-mode rainbow-delimiters projectile ivy-rich helpful general evil-collection evil-cleverparens doom-themes doom-modeline counsel)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
