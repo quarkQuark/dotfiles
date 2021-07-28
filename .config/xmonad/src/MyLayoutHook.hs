b60347eac4212711a21fc8f06e82223785830b6d
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2021-07-28 16:39:14 +0100

    Add Master and Tabs, increase spacing

--- .config/xmonad/src/MyLayoutHook.hs
+++ .config/xmonad/src/MyLayoutHook.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE NoMonomorphismRestriction, FlexibleContexts #-}
-
 module MyLayoutHook
 (myLayoutHook)
 where
 
 import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Layout
+import XMonad.Layout.Master
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.NoBorders
@@ -21,10 +20,9 @@ import Options
 -- Changes only seem to apply if I log out then in again
 -- Dimensions are given as (Border top bottom right left)
 mySpacing = spacingRaw True                -- Only for >1 window
-                       -- The bottom edge seems to look narrower than it is
-                       (Border 0 15 10 10) -- Size of screen edge gaps
+                       (Border 0 20 20 20) -- Size of screen edge gaps
                        True                -- Enable screen edge gaps
-                       (Border 5 5 5 5)    -- Size of window gaps
+                       (Border 8 8 8 8)    -- Size of window gaps
                        True                -- Enable window gaps
 
 myTabConfig :: Theme
@@ -37,6 +35,8 @@ myTabConfig = def { fontName            = myFont
                   , inactiveTextColor   = myTabInactiveTextColour
                   }
 
+myTabbed = tabbed shrinkText myTabConfig
+
 tall  = renamed [Replace "Tall"]
       $ mySpacing
       $ avoidStruts
@@ -48,10 +48,14 @@ three = renamed [Replace "Three"]
       $ ThreeColMid 1 (3/100) (1/2)
 
 tabs  = renamed [Replace "Tabs"]
-      $ avoidStruts
-      $ tabbed shrinkText myTabConfig
+      $ avoidStruts myTabbed
+
+masterAndTabs = renamed [Replace "Master and Tabs"]
+              $ mySpacing
+              $ avoidStruts
+              $ mastered (1/100) (1/2) myTabbed
 
 myLayoutHook = smartBorders
 --           $ mySideDecorate  -- Messes up everything - I don't yet understand why
              $ mkToggle (single NBFULL)
-             $ tall ||| three ||| tabs
+             $ tall ||| three ||| tabs ||| masterAndTabs
