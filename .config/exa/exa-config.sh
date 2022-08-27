b4048ac659413c3d77833af6ebf8f0e6898fac1c
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2022-08-27 12:14:42 +0100

    Exa config is just an environment variable

--- .config/exa/exa-config.sh
+++
@@ -1,7 +0,0 @@
-# Disable highlighting for current user and group
-export EXA_COLORS="uu=38;5;250:gu=38;5;250"
-
-# Make directories a more noticeable shade of blue
-export EXA_COLORS+=":di=38;5;39"
-
-export EXA_COLORS+=":da=36"
